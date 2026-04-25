97c388606ef9ebbea9182520cd263a32fcefd13e
Devika <devikamule8@gmail.com>
2026-04-25 17:44:09 +0530

    Fixed Selenium Docker issue with headless Chrome

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome()
+# 🔹 Configure headless Chrome
+options = Options()
+options.add_argument("--headless")  # run without GUI
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 
 driver.get("http://localhost:5000")
-time.sleep(2)
+time.sleep(3)
 
-# Enter name
+# 🔹 Enter name
 driver.find_element(By.NAME, "name").send_keys("Devika")
 driver.find_element(By.TAG_NAME, "button").click()
 
